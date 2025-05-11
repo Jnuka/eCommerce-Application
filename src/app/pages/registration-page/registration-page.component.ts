aa80cfca5efa9be5177262ccca4061b65e02d359
Jnuka <jnuka@mail.ru>
2025-05-11 15:29:18 +0400

    feat: add login and registration template

--- src/app/pages/registration-page/registration-page.component.ts
+++ src/app/pages/registration-page/registration-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-registration-page',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    NgForOf,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.css',
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class RegistrationPageComponent {}
+export class RegistrationPageComponent {
+  public country = [
+    { value: 'opt-1', viewValue: 'Option 1' },
+    { value: 'opt-2', viewValue: 'Option 2' },
+    { value: 'opt-3', viewValue: 'Option 3' },
+  ];
+}
