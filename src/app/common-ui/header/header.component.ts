4a7dd0dc3cb41f28227ab3fae50456ad1e717e45
Jnuka <jnuka@mail.ru>
2025-05-16 22:02:15 +0400

    feat: add logout button on header

--- src/app/common-ui/header/header.component.ts
+++ src/app/common-ui/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   private router = inject(Router);
+  private authService = inject(AuthService);
+
+  public isAuthAcc(): boolean {
+    return this.authService.isAuth;
+  }
+
+  public logout(): void {
+    this.authService.logout();
+  }
 
   public async goHome(): Promise<void> {
     await this.router.navigate(['']);
