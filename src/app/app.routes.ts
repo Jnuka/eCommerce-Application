2153a536e406f89a5589d1381a8ccded38091357
Jnuka <jnuka@mail.ru>
2025-05-17 22:56:42 +0400

    feat: add catalog and about pages to the router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { CatalogProductPageComponent } from './pages/catalog-product-page/catalog-product-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
 
 export const routes: Routes = [
   {
@@ -13,6 +15,8 @@ export const routes: Routes = [
       { path: '', component: MainPageComponent },
       { path: 'login', component: LoginPageComponent },
       { path: 'registration', component: RegistrationPageComponent },
+      { path: 'catalog', component: CatalogProductPageComponent },
+      { path: 'about', component: AboutUsPageComponent },
       { path: '**', component: NotFoundPageComponent },
     ],
   },
