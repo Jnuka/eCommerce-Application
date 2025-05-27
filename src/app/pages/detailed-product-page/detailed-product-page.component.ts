ad0ee17c816855d9b1bd9f37f162cd0fd52c7518
Jnuka <jnuka@mail.ru>
2025-05-27 12:48:58 +0400

    feat: add selection of different weights for the product and optimization code

--- src/app/pages/detailed-product-page/detailed-product-page.component.ts
+++ src/app/pages/detailed-product-page/detailed-product-page.component.ts
@@ -2,25 +2,36 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductResponse } from '../../products/products.interfaces';
 import { ProductsService } from '../../products/products.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgForOf, NgIf } from '@angular/common';
+import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-detailed-product-page',
-  imports: [],
   templateUrl: './detailed-product-page.component.html',
   styleUrl: './detailed-product-page.component.css',
+  imports: [NgIf, NgForOf, MatButtonToggleGroup, MatButtonToggle],
 })
 export class DetailedProductPageComponent implements OnInit {
   public pageNum: string | null = '';
   public description = '';
   public products: ProductResponse | undefined;
 
+  public srcImage: { url: string; alt: string }[] | undefined;
   public totalPrice: string | undefined;
   public discount: string | undefined;
+  public attributes: { name: string; degree: string }[] | undefined;
 
   public productService = inject(ProductsService);
   private router = inject(Router);
 
+  private isHighWeight = false;
+
   constructor(private activatedRoute: ActivatedRoute) {}
+
+  public changeInToggleGroup(): void {
+    this.isHighWeight = !this.isHighWeight;
+    this.getParameters();
+  }
 
   public ngOnInit(): void {
     this.pageNum = this.activatedRoute.snapshot.queryParamMap.get('productId');
@@ -41,14 +52,86 @@ export class DetailedProductPageComponent implements OnInit {
 
   public getParameters(): void {
     if (this.products) {
-      this.totalPrice = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
-        this.products.masterData.current.masterVariant.prices[0].value.centAmount / 100,
-      );
-      if (this.products.masterData.current.masterVariant.prices[0].discounted) {
-        this.discount = new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
-          this.products.masterData.current.masterVariant.prices[0].discounted.value.centAmount /
-            100,
-        );
+      // Атрибуты продуктов
+      this.attributes = [{ name: '', degree: '' }];
+      this.attributes.shift();
+      for (let i = 0; i < 4; i++) {
+        if (i === 3)
+          this.attributes.push({
+            ['name']: `${this.products.masterData.current.masterVariant.attributes[i].name}`,
+            /* eslint-disable */
+            ['degree']: `${this.products.masterData.current.masterVariant.attributes[i].value}`,
+            /* eslint-enable */
+          });
+        else
+          this.attributes.push({
+            ['name']: `${this.products.masterData.current.masterVariant.attributes[i].name}`,
+            ['degree']: `${this.products.masterData.current.masterVariant.attributes[i].value.label}`,
+          });
+      }
+      /* eslint-disable */
+      this.products.masterData.current.masterVariant.attributes[0].value;
+      /* eslint-enable */
+
+      this.srcImage = [{ url: '', alt: '' }];
+      this.srcImage.shift();
+
+      if (this.isHighWeight) {
+        // Картинка
+        this.srcImage.push({
+          ['url']: `${this.products.masterData.current.variants[0].images[0].url}`,
+          ['alt']: `${this.products.masterData.current.variants[0].images[0].label}`,
+        });
+        // Цена продукта
+        this.totalPrice = new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(this.products.masterData.current.variants[0].prices[0].value.centAmount / 100);
+        // Скидка на продукт, если есть
+        if (this.products.masterData.current.masterVariant.prices[0].discounted) {
+          this.discount = new Intl.NumberFormat('en', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(
+            this.products.masterData.current.variants[0].prices[0].discounted.value.centAmount /
+              100,
+          );
+        }
+        // Атрибут для веса
+        this.attributes.push({
+          ['name']: `${this.products.masterData.current.variants[0].attributes[4].name}`,
+          /* eslint-disable */
+          ['degree']: `${this.products.masterData.current.variants[0].attributes[4].value}`,
+          /* eslint-enable */
+        });
+      } else {
+        // Картинка
+        this.srcImage.push({
+          ['url']: `${this.products.masterData.current.masterVariant.images[0].url}`,
+          ['alt']: `${this.products.masterData.current.masterVariant.images[0].label}`,
+        });
+        // Цена продукта
+        this.totalPrice = new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(this.products.masterData.current.masterVariant.prices[0].value.centAmount / 100);
+        // Скидка на продукт, если есть
+        if (this.products.masterData.current.masterVariant.prices[0].discounted) {
+          this.discount = new Intl.NumberFormat('en', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(
+            this.products.masterData.current.masterVariant.prices[0].discounted.value.centAmount /
+              100,
+          );
+        }
+        // Атрибут для веса
+        this.attributes.push({
+          ['name']: `${this.products.masterData.current.masterVariant.attributes[4].name}`,
+          /* eslint-disable */
+          ['degree']: `${this.products.masterData.current.masterVariant.attributes[4].value}`,
+          /* eslint-enable */
+        });
       }
     }
   }
