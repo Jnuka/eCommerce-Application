226e76f9963f5a816635c03c2ab95ee2b55b6e10
Jnuka <jnuka@mail.ru>
2025-05-27 18:40:36 +0400

    feat: add close button on modal slider

--- src/app/common-ui/modal-window/modal-window.component.ts
+++ src/app/common-ui/modal-window/modal-window.component.ts
@@ -2,13 +2,13 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ImageSliderComponent } from '../image-slider/image-slider.component';
 import { SliderInterface } from '../interfaces/slider.interface';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-window',
   templateUrl: './modal-window.component.html',
   styleUrl: './modal-window.component.css',
-  imports: [MatButtonModule, ImageSliderComponent],
+  imports: [MatButtonModule, ImageSliderComponent, MatDialogActions, MatDialogClose],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalWindowComponent {
